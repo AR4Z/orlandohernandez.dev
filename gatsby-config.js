f3cacabf09c1d4be2c8628622b7026d6ac781058
Orlando Hernandez <oaraz02@gmail.com>
2020-06-01 21:35:10 -0500

    basic structure

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    siteMetadata: {
+        title: `Orlando Hernandez Blog`,
+        author: `Orlando Hernandez`,
+        description: `My personal web page`,
+    },
+    plugins: [
+        `gatsby-transformer-remark`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/content`,
+                name: `content`,
+            },
+        },
+    ],
+};
