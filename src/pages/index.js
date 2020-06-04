5020cd7d30647ed7a2b78790c8908ddd43c6f107
Orlando Hernandez <oaraz02@gmail.com>
2020-06-04 11:44:57 -0500

    add posts list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,36 @@
 /** @jsx jsx */
 import React from 'react';
 import { Heading } from 'theme-ui'
-import { Link, Container } from 'theme-ui'
+import { Link, Container, Styled } from 'theme-ui'
 import { jsx } from "theme-ui"
+
 export default function HomePage() {
+  const posts = [
+    {
+      id: 1,
+      slug: 'test',
+      date: '2020/06/04',
+      title: 'Est ex nulla duis nisi esse duis Lorem do amet dolore dolore.',
+      description: 'Pariatur nisi pariatur qui nostrud velit laborum aute labore ipsum excepteur culpa.',
+      body: 'Laboris ipsum eiusmod et amet. Ipsum aute dolore qui laboris tempor minim voluptate est. Velit eu Lorem ipsum veniam incididunt. Sint quis eiusmod ullamco cillum voluptate. Dolor do occaecat excepteur aliqua et occaecat tempor laborum dolor excepteur consectetur.'
+    },
+    {
+      id: 2,
+      slug: 'test',
+      date: '2020/06/04',
+      title: 'Est ex nulla duis nisi esse duis Lorem do amet dolore dolore.',
+      description: 'Pariatur nisi pariatur qui nostrud velit laborum aute labore ipsum excepteur culpa.',
+      body: 'Laboris ipsum eiusmod et amet. Ipsum aute dolore qui laboris tempor minim voluptate est. Velit eu Lorem ipsum veniam incididunt. Sint quis eiusmod ullamco cillum voluptate. Dolor do occaecat excepteur aliqua et occaecat tempor laborum dolor excepteur consectetur.'
+    },
+    {
+      id: 3,
+      slug: 'test',
+      date: '2020/06/04',
+      title: 'Est ex nulla duis nisi esse duis Lorem do amet dolore dolore.',
+      description: 'Pariatur nisi pariatur qui nostrud velit laborum aute labore ipsum excepteur culpa.',
+      body: 'Laboris ipsum eiusmod et amet. Ipsum aute dolore qui laboris tempor minim voluptate est. Velit eu Lorem ipsum veniam incididunt. Sint quis eiusmod ullamco cillum voluptate. Dolor do occaecat excepteur aliqua et occaecat tempor laborum dolor excepteur consectetur.'
+    }
+  ]
   return (
     <div
       sx={{
@@ -61,8 +88,43 @@ export default function HomePage() {
           flexGrow: 1,
           height: '100%'
         }}>
-        Centered container
-</Container>
+        <ul
+          sx={{
+            listStyle: 'none',
+            m: 0,
+            px: 3,
+            py: 4,
+          }}>
+          {posts.map(post => (
+            <li key={post.id}
+              sx={{
+                mb: 4,
+              }}>
+              <Styled.h2
+                sx={{
+                  m: 0,
+                }}>
+                <Link to={post.slug}
+                  sx={{
+                    color: 'inherit',
+                    textDecoration: 'none',
+                    ':hover,:focus': {
+                      color: 'primary',
+                      textDecoration: 'underline',
+                    }
+                  }}>
+                  {post.title}
+                </Link>
+              </Styled.h2>
+              <small sx={{ fontWeight: 'bold' }}>{post.date}</small>
+              <Styled.p>
+                {post.description}
+              </Styled.p>
+            </li>
+          ))}
+        </ul>
+
+      </Container>
       <footer
         sx={{
           display: 'flex',
